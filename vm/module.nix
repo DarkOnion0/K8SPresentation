1373b76b0bcb04040c1955d78d50c4606a709b39
DarkOnion0 <darkonion0@clapot.org>
2023-03-21 21:13:31 +0100

    build(nix/vm): add new configs

--- vm/module.nix
+++ vm/module.nix
@@ -1,4 +1,13 @@
 {pkgs, ...}: {
+  env = {
+    KUBECONFIG = "./k3s.yaml";
+  };
+
+  hosts = {
+    "kubemaster.local" = "192.168.56.101";
+    "kubeworker.local" = "192.168.56.102";
+  };
+
   packages = with pkgs; [
     # K8S toolings
     kubectl
